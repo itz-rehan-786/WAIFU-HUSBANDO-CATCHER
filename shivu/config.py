4010a670478484aec1c6f3709eb5259fb25bff25
Rehan Khan <151330013+itz-rehan-786@users.noreply.github.com>
2024-10-19 11:29:55 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,19 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "7933409218:AAEmySP4Em63xqHFMvGHWYs2TBYx3AVum5Q"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
-
+OWNER_ID = "7524407228"
+sudo_users = ["2054990632", "6606949931" , "6590287973" ,"6839571494" ,"1648076207"]
+GROUP_ID = "-1001720134719"
+TOKEN = "7500848496:AAG_qEjAZkzRCODLnjjDj7kg6tqyJjQVv00"
+mongo_url = "mongodb+srv://wangling01:wangling@cluster0.trgu2ty.mongodb.net/?retryWrites=true&w=majority"
+PHOTO_URL =  ["https://graph.org/file/f10bec6ec695bba69037d.jpg","https://graph.org/file/bdac0dcb5d841263a8866.jpg","https://telegra.ph/file/75e758e6e47453d301fcd.jpg","https://telegra.ph/file/75e758e6e47453d301fcd.jpg","https://telegra.ph/file/dba87ad2a7d957588b8de.jpg","https://telegra.ph/file/765c9474d372cf40621da.jpg","https://telegra.ph/file/1e55c00d0be79b7ca614d.jpg"]
+SUPPORT_CHAT = "testbotarea"
+UPDATE_CHAT = "bot_support_arena"
+BOT_USERNAME = "Testjdjdjdjdbot"
+CHARA_CHANNEL_ID = "-1002109838600"
+api_id = "20457610"
+api_hash = "b7de0dfecd19375d3f84dbedaeb92537"
     
 class Production(Config):
     LOGGER = True
